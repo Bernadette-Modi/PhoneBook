b8c2d0dd99f5f6a4e317825c758c6068ec9d00c4
Bernadette Modi <berniemain353@gmail.com>
2025-03-05 22:34:00 +0300

    Defined the add contact function to add the contacts.

--- phonebook.py
+++ phonebook.py
@@ -1,5 +1,5 @@
 contacts = {}
-def display_menu: 
+def display_menu(): 
     print("Welcome to the Phone Book system.")
     print("1. Add Contact")
     print("2. View Contacts")
@@ -7,7 +7,21 @@ def display_menu:
     print("4. Update Contact")
     print("5. Delete Contact")
     print("6. Exit")
+
+def add_contact():
+    name = input("First Name: ").strip().title()
+    lname = input("Last Name: ").strip().title()
+    full_name = name + " " + lname
+    if full_name in contacts:
+        print("Contact already exists!")
+        return
+    phine = input("Phone Number: ")
+    contacts[full_name] = phone
+    print(f"Contact {full_name} added sucessfully.")
+
+def view_contact():
     
+
 
 while True:
     record = input("Name: ")
