ca2921c695813b856967c2238fa856cd00e99249
Bernadette Modi <berniemain353@gmail.com>
2025-03-05 21:53:12 +0300

    Created a display menu for the user.

--- phonebook.py
+++ phonebook.py
@@ -1,4 +1,14 @@
 contacts = {}
+def display_menu: 
+    print("Welcome to the Phone Book system.")
+    print("1. Add Contact")
+    print("2. View Contacts")
+    print("3. Search Contact")
+    print("4. Update Contact")
+    print("5. Delete Contact")
+    print("6. Exit")
+    
+
 while True:
     record = input("Name: ")
     num = input("Number: ")
