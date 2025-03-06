067e362af2bf618d47da46caef6300f0f08376ca
Bernadette Modi <berniemain353@gmail.com>
2025-03-06 12:04:17 +0300

    Fixed a few issues like spelling error.

--- phonebook.py
+++ phonebook.py
@@ -15,26 +15,33 @@ def add_contact():
     if full_name in contacts:
         print("Contact already exists!")
         return
-    phine = input("Phone Number: ")
+    phone = input("Phone Number: ")
     contacts[full_name] = phone
     print(f"Contact {full_name} added sucessfully.")
 
 def view_contact():
-    if not contact:
+    if not contacts:
         print("Contact does not exist.")
     else: 
         print("\n --- CONTACT LIST ---")
         for name, phone in contacts.items():
-            print(f"{full_name}: {phone}")
+            print(f"{name}: {phone}")
         print("-----------------------")
 
 def search_contact():
     search = input("Enter the name to search: ").strip().title()
-    for full_name, phone in contacts.item():
+    for full_name, phone in contacts.items():
         if search in name:
             print(f"{full_name}: {phone}")
             return
     print("Contact not found.")
+
+def update_contact():
+    name = input("Enter the name to update: ").strip().title()
+    if name in contacts:
+        new_number = input("Enter new phone number: ").strip()
+        
+
         
 
     
